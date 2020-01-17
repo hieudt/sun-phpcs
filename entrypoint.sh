b51eeb0e0f2a8013d8350f386a1347619603bfa0
hieudt-2054 <55786352+hieudt-2054@users.noreply.github.com>
2020-01-17 10:10:15 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/sh -l
+set -e
 
-echo "running phpcs"
-echo phpcs --standard=Framgia app
+echo "[#0] Logging Current Folder"
+pwd
+echo "[#1] Show list folder "
+ls -la
+echo "[#2] Checking PHP CS Version"
+phpcs -i
+echo "[#3] running phpcs"
+if [ -z "$1" ]; then
+ hieudt="app"
+else 
+ hieudt="$@"
+fi
+
+phpcs --standard=Framgia "$hieudt"
+
+
